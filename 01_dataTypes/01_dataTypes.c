29d172202350f5d34dde417d58b246f752134a39
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-13 17:58:43 +0530

    Data Types
    Data Types in C language.

---
+++ 01_dataTypes/01_dataTypes.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main()
+{
+    int a = 54;
+    float b = 4.54;
+    char c = 'a';
+    double d = 5.23654;
+    bool e = true;
+
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%d\n",e);
+
+    return 0;
+
+}
