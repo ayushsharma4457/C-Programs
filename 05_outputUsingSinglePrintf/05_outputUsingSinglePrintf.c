5bd4fc65cfdd71524e370bb809d74217f4fe30e1
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-14 19:16:36 +0530

    Output Using Single Printf
    Output Using Single Printf.

---
+++ 05_outputUsingSinglePrintf/05_outputUsingSinglePrintf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int maths = 90, physics = 77, chemistry = 69;
+
+    printf("Maths = %d\nPhysics = %d\nChemistry = %d", maths, physics, chemistry);
+
+    return 0;
+}
