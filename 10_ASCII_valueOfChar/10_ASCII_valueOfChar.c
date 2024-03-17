513c12e0ce89c1979a0b0c6785f04cab9fa5b8b7
ayushsharma4457 <150591371+ayushsharma4457@users.noreply.github.com>
2024-03-17 13:28:15 +0530

    Char To ASCII
    read char and print ACSII value.

---
+++ 10_ASCII_valueOfChar/10_ASCII_valueOfChar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char character;
+
+    printf("ENter The Character: ");
+    scanf("%c", &character);
+
+    printf("ASCII Value: %d", character);
+
+    return 0;
+}
